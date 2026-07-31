7f75f50b21b0929f84274569a0645354a9c00d20
Atharv105 <atharvdhandale5@gmail.com>
2026-07-31 15:37:15 +0530

    add logger(Winston/Morgan) and some other changes

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -1,6 +1,10 @@
 
+const logger = require("../utils/logger");
+
 const errorMiddleware = (err, req, res, next) => {
-    console.error(err);
+    logger.error(`${err.message || "Internal Server Error"} - ${req.method} ${req.originalUrl} - IP: ${req.ip}`, {
+        stack: err.stack,
+    });
 
     res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
