f6e2aca4716c0ff0b9c0e108173fc8029c4f7a11
Rushikesh Kharage <rushikeshkharage04@gmail.com>
2026-08-06 21:43:07 +0530

    offer banner on the landing page and img button for the offer banner

--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -2,13 +2,14 @@
 const logger = require("../utils/logger");
 
 const errorMiddleware = (err, req, res, next) => {
-    logger.error(`${err.message || "Internal Server Error"} - ${req.method} ${req.originalUrl} - IP: ${req.ip}`, {
+    const errorDetails = err.original?.message || err.parent?.message || err.message || "Internal Server Error";
+    logger.error(`${errorDetails} - ${req.method} ${req.originalUrl} - IP: ${req.ip}`, {
         stack: err.stack,
     });
 
     res.status(err.statusCode || 500).json({
         message: err.message || "Internal Server Error",
-        error: err.message,
+        error: errorDetails,
         data : null,
     });
 };
